1b596fd5f089cca0a9a7582d5771e6c57a0bfd8e
jeoniee <cmils@naver.com>
2024-12-29 23:57:18 +0900

    model, messages 받는 생성자 추가

--- backend/src/main/java/org/fork/model/ChatGPTRequest.java
+++ backend/src/main/java/org/fork/model/ChatGPTRequest.java
@@ -6,6 +6,16 @@ public class ChatGPTRequest {
     private String model;
     private List<Message> messages;
 
+    // 기본 생성자
+    public ChatGPTRequest() {}
+
+    // model과 messages를 인자로 받는 생성자 추가
+    public ChatGPTRequest(String model, List<Message> messages) {
+        this.model = model;
+        this.messages = messages;
+    }
+
+    //Getter Setter
     public String getModel() {
         return model;
     }
